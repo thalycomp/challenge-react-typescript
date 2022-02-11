73b37717ba205d9b1fb86f7e26c17840a60eeda2
thalyta <thalycomp@gmail.com>
2022-02-11 14:58:13 -0300

    excluindo json

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -3,32 +3,6 @@ import './styles.scss';
 import CardUsers from '../../components/CardUsers';
 import { useGlobalContext } from '../../hooks/context';
 
-const cardUsersData = [
-  {
-    id: 1,
-    name: 'Kurtis Weissnat',
-    username: 'Elwyn.Skiles',
-    email: 'Telly.Hoeger@billy.biz',
-    address: {
-      street: 'teste',
-      suite: 'teste',
-      city: 'teste',
-      zipcode: 2392930,
-      geo: {
-        lat: 23992,
-        lng: 20932,
-      },
-    },
-    phone: 'teste',
-    website: 'teste',
-    company: {
-      name: 'Johns Group',
-      catchPhrase: 'Configurable multimedia task-force',
-      bs: 'teste',
-    },
-  },
-];
-
 const Main: React.FC = () => {
   const { users } = useGlobalContext();
 
