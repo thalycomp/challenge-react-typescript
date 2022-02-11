f34a31648076b8874eafc4773059f337a77561a6
thalyta <thalycomp@gmail.com>
2022-02-11 14:59:05 -0300

    adicionando getPosts

--- src/hooks/context.tsx
+++ src/hooks/context.tsx
@@ -1,11 +1,12 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { api } from '../services/api';
-import { AppGlobalProps, IProviderProps, IUsers } from '../types';
+import { AppGlobalProps, IPosts, IProviderProps, IUsers } from '../types';
 
 const useAppGlobalContext = createContext({} as AppGlobalProps);
 
 export const AppProvider = ({ children }: IProviderProps) => {
   const [users, setUsers] = useState<IUsers[] | []>([]);
+  const [posts, setPosts] = useState<IPosts[] | []>([]);
 
   useEffect(() => {
     async function fetchUsers() {
@@ -22,11 +23,20 @@ export const AppProvider = ({ children }: IProviderProps) => {
     } catch (err) {}
   }
 
+  async function getPosts(userId: number) {
+    try {
+      const { data } = await api.get(`/posts/${userId}`);
+      console.log(data);
+      setPosts(data);
+    } catch (err) {}
+  }
+
   return (
     <useAppGlobalContext.Provider
       value={{
         users,
-        setUsers,
+        getPosts,
+        posts,
       }}
     >
       {children}
