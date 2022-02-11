5b4aed12ba1c6ea1a15d9df1f6ad94be16a35ef4
thalyta <thalycomp@gmail.com>
2022-02-11 14:57:44 -0300

    adicionando types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,27 +1,32 @@
 export interface IUsers {
-  user: {
-    id: number;
-    name: string;
-    username: string;
-    email: string;
-    address: {
-      street: string;
-      suite: string;
-      city: string;
-      zipcode: number;
-      geo: {
-        lat: number;
-        lng: number;
-      };
-    };
-    phone: string;
-    website: string;
-    company: {
-      name: string;
-      catchPhrase: string;
-      bs: string;
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: number;
+    geo: {
+      lat: number;
+      lng: number;
     };
   };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export interface IPosts {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
 }
 
 export interface IProviderProps {
@@ -30,5 +35,6 @@ export interface IProviderProps {
 
 export interface AppGlobalProps {
   users: IUsers[] | [];
-  setUsers: (arg: IUsers[] | []) => void;
+  posts: IPosts[] | [];
+  getPosts: (userId: number) => void;
 }
